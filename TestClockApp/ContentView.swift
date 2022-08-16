d678c160551df766f938c9f0f01dffed87bc9d30
yuta-yama-1123 <yutaforios@gmail.com>
2022-08-16 16:03:47 +0900

    Initial Commit

---
+++ TestClockApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestClockApp
+//
+//  Created by 山本祐太 on 2022/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestClockApp/TestClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestClockAppApp.swift
+//  TestClockApp
+//
+//  Created by 山本祐太 on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct TestClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
